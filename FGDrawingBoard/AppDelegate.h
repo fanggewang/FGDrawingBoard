ea34ab863c7813638a4c9e71d84dc1525ee57c9d
fanggewang <18727582411@163.com>
2016-05-10 15:26:51 +0800

    Initial Commit

---
+++ FGDrawingBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FGDrawingBoard
+//
+//  Created by 王放歌 on 16/5/10.
+//  Copyright © 2016年 WangFangGe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FGDrawingBoard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FGDrawingBoard
+//
+//  Created by 王放歌 on 16/5/10.
+//  Copyright © 2016年 WangFangGe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
