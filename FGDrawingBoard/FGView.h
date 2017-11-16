3190b6f7c5089281137b004100518194af4c2c0d
fanggewang <18727582411@163.com>
2017-11-16 18:52:57 +0800

    创建view

---
+++ FGDrawingBoard/FGView.h
@@ -0,0 +1,13 @@
+//
+//  FGView.h
+//  FGDrawingBoard
+//
+//  Created by 王放歌 on 2017/11/16.
+//  Copyright © 2017年 WangFangGe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FGView : UIView
+
+@end
---
+++ FGDrawingBoard/FGView.m
@@ -0,0 +1,21 @@
+//
+//  FGView.m
+//  FGDrawingBoard
+//
+//  Created by 王放歌 on 2017/11/16.
+//  Copyright © 2017年 WangFangGe. All rights reserved.
+//
+
+#import "FGView.h"
+
+@implementation FGView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
